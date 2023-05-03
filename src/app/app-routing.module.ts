6cc44d1a491f359f8fa8947f72d5743d2323a7db
LeeMeiLing <leem0060@outlook.com>
2023-05-03 08:50:48 +0800

    nested route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product.component';
+import { ProductDetailsComponent } from './components/product-details.component';
+import { ProductInfoComponent } from './components/product-info.component';
+import { ProductSpecsComponent } from './components/product-specs.component';
 
 const routes: Routes = [
-  {path:"", component: HomeComponent},
-  
+  {path:'', component: ProductComponent},
+  {path:'product', component: ProductComponent,
+    children:[
+      {path:'details', component: ProductDetailsComponent, 
+        children:[
+          {path:'info/:pid', component: ProductInfoComponent},
+          {path:'specs', component: ProductSpecsComponent},
+          {path:"**", redirectTo:"details", pathMatch: 'full'}
+        ]},
+    ]},
+  {path:"**", redirectTo:"/", pathMatch: 'full'}
+
 ];
 
 @NgModule({
